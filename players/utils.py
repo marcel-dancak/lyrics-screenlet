516919dd5ad04c5d071dab33133e7a895557b731
Marcel Dancak <dancakm@gmail.com>
2011-02-06 11:51:57 +0100

    * improved converting from URI to local path of playing song

--- players/utils.py
+++ players/utils.py
@@ -1,20 +1,22 @@
+import re
 import gnomevfs
+import traceback
+
+pattern = re.compile("^\w+://")
 
 def get_local_path_from_uri(uri):
 	if uri != None and uri != "":
-		try:			
-			local_path = gnomevfs.get_local_path_from_uri(uri)
-			#print "converting URI to local_path"
-			return local_path
-		except Exception, e:
-			print e
-			print "Cannot convert URI: \"%s\"to local path" % uri
-			print "URI calss: %s" % uri.__class__
+		if not isinstance(uri, basestring):
+			unicodeUri = unicode(str(uri), 'utf-8')
+		
+		match = pattern.match(uri)
+		if match:
 			try:
-				unicodeUri = unicode(str(uri), 'utf-8')
 				local_path = gnomevfs.get_local_path_from_uri(uri)
+				#print "converting URI to local_path"
 				return local_path
 			except Exception, e:
-				print e
-				print "Cannot convert unicoded URI: \"%s\"to local path" % uri
-		return uri
+				print "Converting URI: \"%s\"to local path failed" % uri
+				traceback.print_exc()
+				print "URI calss: %s" % uri.__class__
+	return uri
